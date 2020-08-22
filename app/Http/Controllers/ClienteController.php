c68c2fd7feb8f229ac922181afff394432a89c03
devidy <devidy.oliveira@gmail.com>
2020-08-22 17:32:52 -0300

    feat: cria controller e implementa o metodo store com validacao

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ClienteRequest;
+use App\Models\Cliente;
+
+use Illuminate\Http\Request;
+use App\Http\Services\ClienteService;
+use Symfony\Component\HttpFoundation\Response;
+
+class ClienteController extends Controller
+{
+    protected $service;
+
+    public function __construct(ClienteService $service){
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ClienteRequest $request)
+    {
+        return Cliente::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->service->getClienteById($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
