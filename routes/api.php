d8ae0e1a1114c70b96ecf17184d8d270447aa6d1
devidy <devidy.oliveira@gmail.com>
2020-08-22 17:31:39 -0300

    feat: cria rotas do cliente

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/clientes', 'ClienteController@index');
+Route::get('/clientes/{cliente}', 'ClienteController@show');
+Route::post('/clientes', 'ClienteController@store');
+Route::put('/clientes/{cliente}', 'ClienteController@update');
+Route::delete('/clientes/{cliente}', 'ClienteController@destroy');
