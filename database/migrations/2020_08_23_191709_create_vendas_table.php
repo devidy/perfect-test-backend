e146eb6b214f5653de8d8231c98139c77f2d65e2
devidy <devidy.oliveira@gmail.com>
2020-08-23 16:31:53 -0300

    feat: cria miration de vendas

---
+++ database/migrations/2020_08_23_191709_create_vendas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->foreignId('cliente_id')->constrained('clientes');
+            $table->integer('quantidade');
+            $table->float('desconto', 10, 2);
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendas');
+    }
+}
