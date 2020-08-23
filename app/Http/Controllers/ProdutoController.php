33c5fd929393120bd190a2175d3db5851c870c09
devidy <devidy.oliveira@gmail.com>
2020-08-23 15:35:26 -0300

    feat: implementa metodo para obter o produto por id

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -42,9 +42,9 @@ class ProdutoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Produto $produto)
     {
-        //
+        return new ProdutoResource($produto);
     }
 
     /**
