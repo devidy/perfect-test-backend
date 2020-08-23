b26a0ec12301130cbb7b401619b0475e70e7d872
devidy <devidy.oliveira@gmail.com>
2020-08-23 15:16:10 -0300

    feat: implementa metodo para obter todos os produtos

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use App\Http\Resources\Produto as ProdutoResource;
+use App\Http\Resources\ProdutoCollection;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $produtos = Produto::paginate(10);
+        return (new ProdutoCollection($produtos))
+                    ->response()
+                    ->setStatusCode(Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+}
