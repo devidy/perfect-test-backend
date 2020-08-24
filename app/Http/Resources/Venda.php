c1fd9582ed572f148370c37f5ceb8b60c9e137f9
devidy <devidy.oliveira@gmail.com>
2020-08-24 02:18:19 -0300

    feat: add metodo para transformar status em string

--- app/Http/Resources/Venda.php
+++ app/Http/Resources/Venda.php
@@ -18,11 +18,24 @@ class Venda extends JsonResource
             'id' => $this->id,
             "quantidade" => $this->quantidade,
             "desconto" => $this->desconto,
-            "status" => $this->status,
+            "status" => $this->getStatusInString(),
             "total" => $this->total,
             "data_venda" => $this->data_venda->format('d-m-Y H:00'),
             "cliente" => $this->cliente,
             "produto" => $this->produto
         ];  
+    }
+
+
+
+    public function getStatusInString()
+    {
+        if($this->status === 0) {
+            return 'Cancelado';
+        } else if ($this->status === 1) {
+            return 'Aprovado';
+        } else {
+            return 'Devolvido';
+        }
     }
 }
